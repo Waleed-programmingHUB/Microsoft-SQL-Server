33e17cc6b5bf7aa2228e06f681861272f84a86f1
Waleed-programmingHUB <waleed.alisarwar336@outlook.com>
2021-12-19 22:44:49 +0500

    Update Learn MS SQL Server.sql

--- Learn MS SQL Server.sql
+++ Learn MS SQL Server.sql
@@ -1,5 +1,15 @@
---Without Constraint
---CREATE DATABASE _DATABASE /* Command is used to Create new database */
+/*
+
+	SQL Query 
+	SQL Stands for Structure Query Language 
+
+*/
+-- CREATE DATABASE _DatabaseName
+
+-- CREATE DATABASE _DATABASE -- Command is used to Create new database
+-- Creating a Table in the Database
+
+-- Without Constraint
 
 --CREATE TABLE Students
 --(
@@ -12,7 +22,9 @@
 --)
 
 --USE StudentsManager
+
 --GO
+-- With Constraint
 --CREATE TABLE Student_2
 --(
 --	StudentId INT NOT NULL,
@@ -25,3 +37,12 @@
 
 -- Create table Record using Primary Key
 
+--CREATE TABLE Student_3
+--(
+--	StudentId INT PRIMARY KEY ,
+--	FirstName nvarchar(50) NOT NULL,
+--	LastName nvarchar(100) NULL,
+--	Email nvarchar(50) NOT NULL,
+--	MobileNumber NVARCHAR(12) NULL,
+--	Notes nvarchar(MAX) NULL,
+--)
