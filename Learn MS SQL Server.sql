c80bdb1819a37549bfd3f6b037174fb8968e847a
Waleed-programmingHUB <waleed.alisarwar336@outlook.com>
2021-12-19 22:33:46 +0500

    SQL Server
    SQL Queries
    @2021 Waleed-programmingHUB

---
+++ Learn MS SQL Server.sql
@@ -0,0 +1,27 @@
+--Without Constraint
+--CREATE DATABASE _DATABASE /* Command is used to Create new database */
+
+--CREATE TABLE Students
+--(
+--	StudentId INT,
+--	FirstName nvarchar(50),
+--	LastName nvarchar(100),
+--	Email nvarchar(50),
+--	MobileNumber NVARCHAR(12),
+--	Notes nvarchar(MAX),
+--)
+
+--USE StudentsManager
+--GO
+--CREATE TABLE Student_2
+--(
+--	StudentId INT NOT NULL,
+--	FirstName nvarchar(50) NOT NULL,
+--	LastName nvarchar(100) NULL,
+--	Email nvarchar(50) NOT NULL,
+--	MobileNumber NVARCHAR(12) NULL,
+--	Notes nvarchar(MAX) NULL,
+--)
+
+-- Create table Record using Primary Key
+
