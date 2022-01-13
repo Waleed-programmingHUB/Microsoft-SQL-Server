4726db4acbdc17b05d52d8629219c43a41004ace
Waleed-programmingHUB <waleed.alisarwar336@outlook.com>
2022-01-14 01:28:36 +0500

    Update Learn MS SQL Server.sql

--- Learn MS SQL Server.sql
+++ Learn MS SQL Server.sql
@@ -98,3 +98,128 @@
 
 ---- printing a record expect to this name are starts with any english name
 --SELECT * FROM Employees WHERE FirstName LIKE '[^SLN]%'
+
+
+/* T-SQL */
+-- BETWEEN , AND keyword or NOT BETWEEN ,AND keyword
+
+-- Example 1
+-- printing a data which we defined to print Employees EmployeeID start 6 to 8 showing the record 
+--SELECT EmployeeID AS ID ,TitleOfCourtesy+ FirstName + ' ' + LastName AS 'Name of Employee' ,Title AS 'Current Position' FROM Employees WHERE EmployeeID BETWEEN 6 AND 8
+
+---- Example 2
+---- printing a data which we defined to print Employees EmployeeID start 6 to 8 not showing  in this record 
+--SELECT * FROM Employees WHERE EmployeeID NOT BETWEEN 6 AND 8 -- it will print all the record expect to 6 to 8 
+
+---- Example 3
+---- Sequence the record -> ORDER BY _typeData
+---- printing a Product price
+--SELECT ProductID AS ID ,ProductName ,UnitPrice AS PRICE FROM [dbo].[Products] WHERE UnitPrice BETWEEN 15 AND 18 ORDER BY UnitPrice
+
+---- Example with Text
+---- find all the items whose products name starts letter :BETWEEN C AND D
+--SELECT ProductID , ProductName FROM Products WHERE ProductName BETWEEN 'C' AND 'D' ORDER BY ProductID
+
+---- Example with Dates
+---- find all the orders which are ordered between 15 days of July 1996
+--SELECT * FROM Orders WHERE OrderDate BETWEEN '07-01-1996' AND '07-15-1996'
+
+---- SQL Functions
+---- There are 2 types of SQL Functions
+---- 1. Aggreigate Function -> GROUP BY
+---- 2. Scalar Function
+---- Aggreigate Function MOST COMMONLY USED
+---- COUNT ,AVG ,SUM ,MAX ,MIN 
+
+---- Example 1 COUNT() the total number of Employees in London
+--SELECT COUNT(*) AS 'Number of Employee in London City' FROM Employees WHERE City = 'London' -- Equal to London
+---- Example 2
+--SELECT COUNT(*) AS 'Number of Employee which is not in the London City' FROM Employees WHERE City <> 'London' -- Not Equal to London
+
+---- Example 1 AVG() find the Average of Products
+--SELECT AVG(UnitPrice) AS 'Average Price' FROM Products
+---- Example 2
+--SELECT * FROM Products WHERE UnitPrice < (SELECT AVG(UnitPrice) FROM Products) ORDER BY ProductID
+---- Example 3 total number of price below whose price is below average price of all products 
+--SELECT COUNT(*) AS 'Number Of Products' FROM Products WHERE UnitPrice < (SELECT AVG(UnitPrice) FROM Products)  
+
+---- Example 1 MIN() find the cheapest product offered by company
+--SELECT MIN(UnitPrice) AS 'minimum unit price of product' FROM Products  
+
+---- CONVERT Function in SQL Server
+---- Syntax
+--SELECT  EmployeeID AS ID, TitleOfCourtesy + firstName + ' ' + LastName AS 'Name of Person' ,
+--CONVERT(VARCHAR,BirthDate ,101) AS BirthDate,
+--HireDate
+--FROM Employees
+
+--/* Creating a NEW DATABASE for STUDENTS */
+--CREATE DATABASE TestDB2Join2
+--Go
+
+--Use TestDB2Join2
+--Go
+
+--CREATE TABLE [dbo].[Students]
+--(
+--	[StudentId] [int] NOT NULL,
+--	[FullName] [nvarchar](50) NULL,
+--	[Email] [nvarchar](50) NULL,
+--	[Mobile] [nvarchar](50) NULL,
+--	[DateOfBirth] [datetime] NULL,
+--	[Notes] [nvarchar](max) NULL,
+--)
+--Go
+
+--CREATE TABLE [dbo].[StudentsAddresses]
+--(
+--	[AddressId] [int] NOT NULL,
+--	[StudentId] [int] NULL,
+--	[Address1] [nvarchar](50) NULL,
+--	[City] [nvarchar](50) NULL,
+--	[Country] [nvarchar](50) NULL,
+--	[PostCode] [nvarchar](50) NULL,
+--)
+--Go
+
+---- Data 
+--INSERT [dbo].[Students] ([StudentId], [FullName], [Email], [Mobile], [DateOfBirth], [Notes]) VALUES (1, N'John Smith', N'j.smith@gmail.com', N'0762589632', CAST(N'2001-11-20T00:00:00.000' AS DateTime), N'x')
+--GO
+--INSERT [dbo].[Students] ([StudentId], [FullName], [Email], [Mobile], [DateOfBirth], [Notes]) VALUES (2, N'Adam Wilson', N'adam.w@abc.com', N'0778523698', CAST(N'1999-12-21T00:00:00.000' AS DateTime), N'x')
+--GO
+--INSERT [dbo].[Students] ([StudentId], [FullName], [Email], [Mobile], [DateOfBirth], [Notes]) VALUES (3, N'James Taylor', N'james.taylor@abc.com', N'0748523698', CAST(N'1996-11-02T00:00:00.000' AS DateTime), N'x')
+--GO
+--INSERT [dbo].[Students] ([StudentId], [FullName], [Email], [Mobile], [DateOfBirth], [Notes]) VALUES (4, N'Tony Griag', N'Tony.g@abc.com', N'0745896325', CAST(N'1995-11-02T00:00:00.000' AS DateTime), N'x')
+--GO
+--INSERT [dbo].[StudentsAddresses] ([AddressId], [StudentId], [Address1], [City], [Country], [PostCode]) VALUES (1, 1, N'123 ABC Road', N'London', N'United Kingdom', N'SW32 2AL')
+--GO
+--INSERT [dbo].[StudentsAddresses] ([AddressId], [StudentId], [Address1], [City], [Country], [PostCode]) VALUES (2, 2, N'34A Jones Street', N'Leeds', N'United Kingdom', N'LE34 3BA')
+--GO
+--INSERT [dbo].[StudentsAddresses] ([AddressId], [StudentId], [Address1], [City], [Country], [PostCode]) VALUES (3, 3, N'45 Taylor Road', N'Aylesburry', N'United Kingdom', N'HE3 3AL')
+--GO
+--INSERT [dbo].[StudentsAddresses] ([AddressId], [StudentId], [Address1], [City], [Country], [PostCode]) VALUES (4, 5, N'23 XYZ Street', N'Glassgow', N'United Kingdom', N'GL2 3MN')
+--GO
+
+
+/* SQL Join ,RIGHT JOIN , LEFT JOIN ,FULL JOIN */
+
+SELECT * FROM [dbo].[Students]
+SELECT * FROM [dbo].[StudentsAddresses]
+
+-- INNER JOIN
+-- Example
+/* INER JOIN */
+SELECT * FROM Students AS Student1_Table
+INNER JOIN StudentsAddresses AS Student2Location_table ON Student1_Table.StudentId = Student2Location_table.StudentId  
+
+/* Left JOIN */
+SELECT * FROM Students LEFT JOIN StudentsAddresses ON Students.StudentId = StudentsAddresses.StudentId
+
+/* Right JOIN */
+SELECT * FROM Students RIGHT JOIN StudentsAddresses ON Students.StudentId = StudentsAddresses.StudentId
+
+/* FULL Join */
+SELECT * FROM Students FULL JOIN StudentsAddresses ON Students.StudentId = StudentsAddresses.StudentId
+
+/* Switch Case SQL */
+
